8b0ab163a357e38acf02c603fd88e543efefd2ea
Cj-ira <wickedcody123@protonmail.com>
2024-04-03 11:02:06 +1030

    Basic Structure.

---
+++ 6502 Emulator/Internals/Cpu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6502_Emulator.Internals
+{
+    public enum Status 
+    {
+        C,
+        Z,
+        I,
+        D,
+        B,
+        V,
+        N
+    }
+
+    public class Cpu 
+    {
+        public ushort PC, SP; // programmer counter, stack pointers
+        byte A, X, Y; // accumulator, index register X, Index register Y
+        BitArray PS; // Processor status.
+        Memory memory; // hard coded for now.
+
+
+        void Reset() 
+        {
+            PC = 0xFFFC;
+            SP = 0x0100;
+            ProssesorSet(Status.C, false);
+            ProssesorSet(Status.Z, false);
+            ProssesorSet(Status.I, false);
+            ProssesorSet(Status.D, false);
+            ProssesorSet(Status.B, false);
+            ProssesorSet(Status.V, false);
+            ProssesorSet(Status.N, false);
+            A = 0; X = 0; Y = 0;
+            memory.Init();
+        }
+
+        bool ProssesorGet(Status status) 
+        {
+            return PS.Get((int)status);
+        }
+
+        void ProssesorSet(Status status, bool value) 
+        {
+            PS.Set((int)status, value);
+        }
+    }
+}
---
+++ 6502 Emulator/Internals/Memory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6502_Emulator.Internals
+{
+    public class Memory
+    {
+        public static uint MAX_MEMORY = 1024 * 64;
+
+        byte[] memory = new byte[MAX_MEMORY];
+
+        public Memory()
+        {
+            
+        }
+
+        public void Init() 
+        {
+            for (int i = 0;  i < MAX_MEMORY; i++) 
+            {
+                memory[i] = 0;
+            }
+        }
+
+    }
+}
