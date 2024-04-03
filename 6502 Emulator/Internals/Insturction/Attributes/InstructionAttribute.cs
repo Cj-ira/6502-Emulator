acc8fe27c4e2cf3d65a5050be42b5f74a812ef9d
Cj-ira <wickedcody123@protonmail.com>
2024-04-03 11:15:30 +1030

    AttributeScanner and Attribute for instructions.

---
+++ 6502 Emulator/Internals/Insturction/Attributes/InstructionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6502_Emulator.Internals.Insturction.Attributes
+{
+    public class InstructionAttribute : Attribute
+    {
+    }
+}
